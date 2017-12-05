29cc147e33911d5adb91fc364ca214c85a14cdff
Jen Wong <jenjwong@Jens-MacBook-Pro.local>
2017-12-05 00:23:00 -0800

    mend

--- 5/5.js
+++ 5/5.js
@@ -3,27 +3,26 @@ const path = require('path');
 
 const filePath = path.join(__dirname, '5Input.js');
 
-const test = [0, 3, 0, 1, -3];
 function parse(data) {
   return data.split('\n');
 }
 
 function countSteps1(list) {
   let i = 0;
-  let count = 0;
+  let steps = 0;
   let jump;
   while (i < list.length) {
     jump = list[i];
     list[i] = parseInt(list[i]) + 1;
     i = parseInt(jump) + i;
-    count++;
+    steps++;
   }
-  return count;
+  return steps;
 }
 
 function countSteps2(list) {
   let i = 0;
-  let count = 0;
+  let steps = 0;
   let jump;
   while (i < list.length) {
     jump = list[i];
@@ -33,12 +32,12 @@ function countSteps2(list) {
       list[i] = parseInt(list[i]) + 1;
     }
     i = parseInt(jump) + i;
-    count++;
+    steps++;
   }
-  return count;
+  return steps;
 }
 
 fs.readFile(filePath, 'utf8', (err, res) => {
-  console.log(countSteps(parse(res)));
+  console.log(countSteps1(parse(res)));
   console.log(countSteps2(parse(res)));
 });
